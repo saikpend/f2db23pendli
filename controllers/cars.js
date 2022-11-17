39e6e904bdb75838e58988d56d3cf3c113ab7838
saikpend <112040612+saikpend@users.noreply.github.com>
2022-11-16 23:32:56 -0600

    updated

--- controllers/cars.js
+++ controllers/cars.js
@@ -126,3 +126,45 @@ exports.cars_view_one_Page = async function(req, res) {
     res.send(`{'error': '${err}'}`);
     }
    };
+
+   // Handle building the view for creating a cars.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.cars_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('carscreate', { title: 'Cars Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+   // Handle building the view for updating a cars. 
+// query provides the id 
+exports.cars_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await cars.findById(req.query.id) 
+        res.render('carsupdate', { title: 'Cars Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle a delete one view with id from query 
+exports.cars_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await cars.findById(req.query.id) 
+        res.render('carsdelete', { title: 'Cars Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
--- routes/cars.js
+++ routes/cars.js
@@ -10,5 +10,14 @@ router.get('/', cars_controlers.cars_view_all_Page);
 
 /* GET detail cars page */
 router.get('/detail', cars_controlers.cars_view_one_Page);
+
+/* GET create cars page */
+router.get('/create', cars_controlers.cars_create_Page);
+
+/* GET create update page */
+router.get('/update', cars_controlers.cars_update_Page);
+
+/* GET delete cars page */ 
+router.get('/delete', cars_controlers.cars_delete_Page); 
  
 module.exports = router;
