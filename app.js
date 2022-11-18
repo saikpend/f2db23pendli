6cf29beefbfdcaab82fe12145bc96844664b960b
saikpend <112040612+saikpend@users.noreply.github.com>
2022-11-18 00:33:10 -0600

    passport athun

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var cars = require("./models/cars");
 require('dotenv').config(); 
 const connectionString =  
@@ -18,6 +20,13 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once("open", function () {	
   console.log("Connection to DB succeeded")	
 });
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -36,6 +45,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -44,6 +60,19 @@ app.use('/cars', carsRouter);
 app.use('/gridbuild', gridbuildRouter);
 app.use('/selector', selctorRouter);
 app.use('/resource', resourceRouter);
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username }, function (err, user) {
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  });
+  }))
 
 
 // catch 404 and forward to error handler
